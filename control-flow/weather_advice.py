c1f1cd5b5660a733d7039584f208e382932ace43
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-06-28 10:08:05 +0300

    Add script to advice on current weather

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+current_weather = int(input("What's the weather like today? (sunny/rainy/cold"))
+
+if current_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+    if current_weather == "rainy":
+        print("Don't forget your umbrella and a raincoat.")
+        if current_weather == "cold":
+            print("Make sure to wear a warm coat and a scarf.")
+        else:
+            print("Sorry, I don't have recommendations for this weather.")
+                          
