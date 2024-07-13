2a334aad10e2c0a06d3418d253d8a8f79b949c4b
Ignatius47 <ignatiusx47@gmail.com>
2024-07-13 09:34:23 +0300

    Add script to perform tasks

--- robust_division_calculator.py
+++ robust_division_calculator.py
@@ -1,10 +1,9 @@
 def safe_divide(numerator, denominator):
-    numerator = float(numerator)
-    denominator = float(denominator)
-    
-    ZeroDivisionError
-    if denominator == 0:
-        return "Error: Dividion by zero is not allowed."
-    
-    ValueError
-    return "Error: Invalid input. Please enter numeric values."
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+        if denominator == 0:
+            return "Error: Division by zero is not allowed."
+        return numerator / denominator
+    except ValueError:
+        return "Error: Invalid input. Please enter numeric values."
