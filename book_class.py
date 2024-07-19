30f25ba11ffb05d99cc5fd83b016ea1e0b09f894
Ignatius47 <ignatiusx47@gmail.com>
2024-07-19 10:23:33 +0300

    Script that adds books

---
+++ book_class.py
@@ -0,0 +1,11 @@
+class Book:
+    def __init__(self, title, author, publication_year):
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+
+    def __del__(self):
+        print(f"Deleting: {self.title}")
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Publication Year: {self.publication_year}"
