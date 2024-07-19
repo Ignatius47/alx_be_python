351e3e111749790d999366b179fc3083f46ecbbf
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-07-19 11:05:23 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,6 +3,9 @@ class Book:
         self.title = title
         self.author = author
         self.pages = 0
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
     
 class EBook(Book):
         def __init__(self, title, author, file_size):
